9842a172a9ab7cd2203d191b2436281c05ddcf4e
Zeb <burroughszeb@me.com>
2025-11-05 13:12:08 -0600

    add japanese

---
+++ resources/lang/ja/routes.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    "home" => "/",
+    "shortcut" => [
+        "download" => "shoutokatto/download/{shortcut}",
+        "create" => "shoutokatto/sakusei",
+        "edit" => "shoutokatto/henkou/{shortcut}",
+        "detail" => "shoutokatto/{shortcut}",
+    ],
+    "post" => [
+        "create" => "toukou/sakusei",
+        "edit" => "toukou/henkou/{post}",
+        "detail" => "toukou/{post}",
+    ],
+    "dashboard" => [
+        "shortcuts" => "dasshubo-do/shoutokatto",
+        "posts" => "dasshubo-do/toukou",
+    ],
+    "profile" => "purofi-ru",
+    "how-to-delete" => "sakujo-houhou",
+    "privacy-policy" => "puraibashii",
+    "tos" => "riyō-kiyaku",
+    "login" => "roguin",
+];
